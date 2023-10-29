e4dff07863bc151ee93901a3d1a23519f527bfe1
Evgeniy321 <81414292+Evgeniy321@users.noreply.github.com>
2023-10-29 23:56:42 +0000

    added git ignore
    added git ignore file to secure token and ids

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import telebot
 from random import choice
+import security as s
 from time import sleep
 from webserver import keep_alive
 
-token = '6816730245:AAFkC7JDGjkRv19SuOyIQ_PPRTZEhdk4enU'
 ready = False
-#input visdom chat_id
-chat_id = -1002009985478
-#input your chat id
-host_id = 1054014624
-#chanal id
-chanal_id = -1002037012553
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(s.token)
 
 
 @bot.message_handler(commands=['start'])
@@ -27,9 +21,9 @@ def new_message(message):
 
 @bot.channel_post_handler()
 def new_channel_message(message):
-  if message.chat.id == chanal_id:  # если бот в нескольких каналах, можно разделять действия по id
+  if message.chat.id == s.chanal_id:  # если бот в нескольких каналах, можно разделять действия по id
       sleep(5)
-      bot.send_message(chat_id, 'Мур мяу *кусь за ногу и валить*')
+      bot.send_message(s.chat_id, 'Мур мяу *кусь за ногу и валить*')
   else:
     bot.send_message(message.chat.id, '*Накоклился в лежаночке и спит*')
     print(message, ready)
