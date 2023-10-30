97636b20c41962e06b87d1379455e5067fc0e7dd
Evgeniy321 <81414292+Evgeniy321@users.noreply.github.com>
2023-10-30 01:31:32 +0000

    add comment activity
    added attack, sleep, greatings and eat actions

--- main.py
+++ main.py
@@ -1,32 +1,83 @@
 import telebot
-from random import choice
+from random import choice, randint
 import security as s
 from time import sleep
 from webserver import keep_alive
 
 ready = False
+actionCount = 0
 bot = telebot.TeleBot(s.token)
 
+attacks_action = ['Мур мяу *кусь за ногу и валить*',
+                  'Кусь за ногe',
+                  'Кусь за палец',
+                  'Кусь за живот',
+                  'Реззко подбигает и просит кушать',
+                  "*Скря-Скряб-Скряб побдегает к тебе варан и ждет пока ты дашь вкусняшку"]
+
+sleep_actions = ['Спит',
+                 'Спит',
+                 'Спит',
+                 'Спит',
+                 'Спит',
+                 'Спит']
+
+sleep_actions_finaly = ['*Ушел в другую комнату спать, вы ему мешайте своими переговорами*']
+
+hi_steackers = ['CAACAgIAAxkBAAEBqWVlPvUNAfcDUGbqWhfruEkJnSk8kAACYAADDnr7CkGRLQbGGfu6MAQ',
+                'CAACAgIAAxkBAAEBqWdlPvUl3DdNUK6SajXC_YyeCairJAACjgADl_TGFGxjxuaUUOw3MAQ',
+                'CAACAgIAAxkBAAEBqWllPvUzAAHRkOa_N0A0CTIFZHG_RO0AAjoUAAKH28lLt4S2aCX5a1QwBA',
+                'CAACAgIAAxkBAAEBqWtlPvVOQds5ho-t9rw47KUTQZEpWgACXgcAAlwCZQNV1K4CElTj3zAE',
+                'CAACAgQAAxkBAAEBqW1lPvVl0WqdGTSwMPMJVsmVTQEMCQACpQADzjkIDUnINzIR1FDaMAQ']
+
+eat_steackers = ['CAACAgIAAxkBAAEBqVtlPvRAYu5ROO-LgCZW5JjhJtkBgQACgg4AAmdfgEl1YzlcXGcPTTAE',
+                 'CAACAgIAAxkBAAEBqV1lPvRjw6aqotx_GG-bymvrkoBhjgACjBAAArrqMEkWokxDvj3lGTAE',
+                 'CAACAgIAAxkBAAEBqV9lPvR1dWi83-hudXReiavKkISmCAACOg0AAir8MUonISBQnYVdfTAE',
+                 'CAACAgIAAxkBAAEBqWFlPvSXwIsrc06sPPNLgxf3Xw1u3gACTQADv2adGE2tU0P-VNGzMAQ',
+                 'CAACAgIAAxkBAAEBqWNlPvSprKe1vnXgUMOq3rgp6wY3BQACPwAD3U7yFffJ8Ga23MjqMAQ']
+
+sleepy_stickers = []
+
+
+def action(bot, message,action_frase, sticker):
+  bot.send_sticker(message.chat.id, choice(sticker), reply_to_message_id=message.message_id)
+  bot.reply_to(message, choice(action_frase))
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id, 'Мур мяу, кусь')
+  bot.send_message(message.chat.id, '*Вы входите в комнату Пети, он крепко спит*')
+  bot.send_message(message.chat.id, choice(sleep_actions_finaly))
     #launch bot for chanal
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 'photo'])
 def new_message(message):
-    #for a new message in channel
-  pass
+  global actionCount
+  if actionCount == 0:
+    if message.chat.id != s.chat_id and message.chat.id != s.chanal_id:#личное сообщение
+      bot.send_message(message.chat.id, '*Тебя же дурака предупреждали, не будить...*')
+      bot.send_sticker(message.chat.id, choice(eat_steackers))
+    elif message.chat.id != s.chanal_id:#первый комментарий к посту
+      action(bot,message, attacks_action, hi_steackers)
+  else:#если он поздаровался, но в коментариях идет бесседа 
+    if randint(0,actionCount+3) == 0:
+      action(bot,message, sleep_actions, sleepy_stickers)
+    elif actionCount > 5:
+      action(bot,message, sleep_actions_finaly, sleepy_stickers)
+      actionCount = -1
+    else:
+      actionCount += 1
+    
+
 
 @bot.channel_post_handler()
 def new_channel_message(message):
-  if message.chat.id == s.chanal_id:  # если бот в нескольких каналах, можно разделять действия по id
-      sleep(5)
-      bot.send_message(s.chat_id, 'Мур мяу *кусь за ногу и валить*')
-  else:
+  global actionCount
+  actionCount = 0
+  if message.chat.id != s.chanal_id:  # если бот в нескольких каналах, можно разделять действия по id
     bot.send_message(message.chat.id, '*Накоклился в лежаночке и спит*')
     print(message, ready)
+
 
 
 keep_alive()
